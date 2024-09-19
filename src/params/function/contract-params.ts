0fa7bbe459d00f88a8097e0c8a86c1aee241fce1
Grafiters <bayugrafit@gmail.com>
2024-09-19 15:53:13 +0700

    refac(interface): remove unused interface class

--- src/params/function/contract-params.ts
+++
@@ -1,8 +0,0 @@
-export class InitContractParams {
-  contract_address: string;
-  staked_token: string;
-  reward_token: string;
-  reward_per_block: string;
-  start_block: number;
-  bonus_end_block: number;
-}
--- src/params/request/payment-stack-request.dto.ts
+++
@@ -1,6 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class PaymentRequest {
-  @ApiProperty({ type: String, required: true })
-  hash: string;
-}
